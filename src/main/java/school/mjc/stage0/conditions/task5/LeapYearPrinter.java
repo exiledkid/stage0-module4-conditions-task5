d16e51a73b667a10c524807c44adfe56a9c43f65
exiledkid <fortunemodspartner@gmail.com>
2022-12-22 02:14:51 +0300

    ,,,

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+
+        boolean a, b, c, d;
+        a = (year % 4 == 0);
+        b = (year % 100 != 0);
+        c = (year % 400 == 0);
+        d = a & (b || c);
+        year = d ? 1 : 0;
+
+        switch (year) {
+            case (1):
+                System.out.println("leap");
+                break;
+
+            case (0):
+                System.out.println("not leap");
+                break;
+        }
     }
 }
